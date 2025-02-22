eec563d0d7119e497b25001d365564e05082681d
tklasnakov <todor.klasnakov@paysafe.com>
2025-02-22 09:27:37 +0200

    add copilot chat

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,12 @@
 return {
-	"github/copilot.vim",
-	enabled = false
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" },
+			{ "nvim-lua/plenary.nvim", branch = "master" },
+		},
+		build = "make tiktoken",
+		opts = {},
+		vim.api.nvim_set_keymap("n", "<leader>cp", ":CopilotChatToggle<CR>", { noremap = true, silent = true }),
+	},
 }
